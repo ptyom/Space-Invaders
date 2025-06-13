e74ddac0023a60e1ccd926dbc6ad4aea927a302c
Noor Ansari <155044062+ptyom@users.noreply.github.com>
2025-06-12 21:46:53 -0400

    correct java version

--- SpaceInvaders.java
+++ SpaceInvaders.java
@@ -1,37 +1,61 @@
-int playerX = 4;
-int enemyX = (int)(Math.random() * 5) + 1;
-int enemyY = 0;
-int bulletX = 0, bulletY = 0;
-boolean bulletActive = false;
+// Noor Ansari
 
-while (true) {
-    drawPixel(playerX, 7, "blue");
-    if (enemyY < 8) drawPixel(enemyX, enemyY, "green");
-    if (bulletActive && bulletY >= 0) drawPixel(bulletX, bulletY, "red");
+import java.util.Random;
 
-    // bullet
-    if (bulletActive) {
-        bulletY--;
-        if (bulletY < 0) bulletActive = false;
-        if (bulletX == enemyX && bulletY == enemyY) {
-            // Hit enemy
-            enemyX = (int)(Math.random() * 5) + 1;
-            enemyY = 0;
-            bulletActive = false;
+public class SpaceInvaders {
+    private int playerX = 4;
+    private int enemyX = randomX();
+    private int enemyY = 0;
+    private int bulletX = 0, bulletY = 0;
+    private boolean bulletActive = false;
+    private boolean gameOver = false;
+
+    private static int randomX() {
+        return new Random().nextInt(5) + 1;
+    }
+
+    public int getPlayerX() { return playerX; }
+    public int getEnemyX() { return enemyX; }
+    public int getEnemyY() { return enemyY; }
+    public int getBulletX() { return bulletX; }
+    public int getBulletY() { return bulletY; }
+    public boolean isBulletActive() { return bulletActive; }
+    public boolean isGameOver() { return gameOver; }
+
+    public void movePlayerLeft() {
+        if (playerX > 0) playerX--;
+    }
+
+    public void movePlayerRight() {
+        if (playerX < 7) playerX++;
+    }
+
+    public void fireBullet() {
+        if (!bulletActive) {
+            bulletX = playerX;
+            bulletY = 6;
+            bulletActive = true;
         }
     }
 
-    // enemy
-    Thread.sleep(600);
-    enemyY++;
-    if (enemyY == 7 && enemyX == playerX) {
-        System.out.println("GAME OVER");
-        break;
+    public void updateBullet() {
+        if (bulletActive) {
+            bulletY--;
+            if (bulletY < 0) {
+                bulletActive = false;
+            } else if (bulletX == enemyX && bulletY == enemyY) {
+                // hit
+                enemyX = randomX();
+                enemyY = 0;
+                bulletActive = false;
+            }
+        }
     }
 
-    if (!bulletActive) {
-        bulletX = playerX;
-        bulletY = 6;
-        bulletActive = true;
+    public void updateEnemy() {
+        enemyY++;
+        if (enemyY == 7 && enemyX == playerX) {
+            gameOver = true;
+        }
     }
 }
