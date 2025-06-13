e49b9a95e61c7811ed96fa6b489e4cf58581d8a4
Noor Ansari <155044062+ptyom@users.noreply.github.com>
2025-06-12 21:44:28 -0400

    Correct space_invaders.py

--- space_invaders.py
+++ space_invaders.py
@@ -1,51 +1,54 @@
-import time
+#Noor Ansari
+
 import random
 
 WIDTH = 8
 HEIGHT = 8
 
-player_x = 4
-enemy_x = random.randint(1, 5)
-enemy_y = 0
-bullet_x = 0
-bullet_y = 0
-bullet_active = False
+class Game:
+    def __init__(self):
+        self.player_x = 4
+        self.enemy_x = random.randint(1, 5)
+        self.enemy_y = 0
+        self.bullet_x = 0
+        self.bullet_y = 0
+        self.bullet_active = False
+        self.game_over = False
 
-while True:
-    screen = [[' ' for _ in range(WIDTH)] for _ in range(HEIGHT)]
-    screen[7][player_x] = 'P'  # Player
-    if 0 <= enemy_y < HEIGHT:
-        screen[enemy_y][enemy_x] = 'E'
-    if bullet_active and 0 <= bullet_y < HEIGHT:
-        screen[bullet_y][bullet_x] = '^'
-    for row in screen:
-        print(''.join(row))
-    print('-' * 10)
+    def move_player_left(self):
+        if self.player_x > 0:
+            self.player_x -= 1
 
-    # Bullet logic
-    if bullet_active:
-        bullet_y -= 1
-        if bullet_y < 0:
-            bullet_active = False
-        elif bullet_x == enemy_x and bullet_y == enemy_y:
-            # hit
-            enemy_x = random.randint(1, 5)
-            enemy_y = 0
-            bullet_active = False
+    def move_player_right(self):
+        if self.player_x < WIDTH - 1:
+            self.player_x += 1
 
-    # Enemy 
-    time.sleep(0.6)
-    enemy_y += 1
-    if enemy_y == 7 and enemy_x == player_x:
-        print("GAME OVER")
-        break
+    def fire_bullet(self):
+        if not self.bullet_active:
+            self.bullet_x = self.player_x
+            self.bullet_y = 6
+            self.bullet_active = True
 
-    if not bullet_active and random.random() < 0.3:
-        bullet_x = player_x
-        bullet_y = 6
-        bullet_active = True
-    move = random.choice(['left', 'right', 'stay'])
-    if move == 'left' and player_x > 0:
-        player_x -= 1
-    elif move == 'right' and player_x < WIDTH - 1:
-        player_x += 1
+    def update_bullet(self):
+        if self.bullet_active:
+            self.bullet_y -= 1
+            if self.bullet_y < 0:
+                self.bullet_active = False
+            elif self.bullet_x == self.enemy_x and self.bullet_y == self.enemy_y:
+                self.enemy_x = random.randint(1, 5)
+                self.enemy_y = 0
+                self.bullet_active = False
+
+    def update_enemy(self):
+        self.enemy_y += 1
+        if self.enemy_y == 7 and self.enemy_x == self.player_x:
+            self.game_over = True
+
+    def get_state(self):
+        return {
+            "player": self.player_x,
+            "enemy": (self.enemy_x, self.enemy_y),
+            "bullet": (self.bullet_x, self.bullet_y),
+            "bullet_active": self.bullet_active,
+            "game_over": self.game_over
+        }
